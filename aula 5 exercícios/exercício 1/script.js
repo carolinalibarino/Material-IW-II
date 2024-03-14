bc52baa92aef440d6f37500940e5daa766defd34
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-03-14 12:20:36 -0300

    Add files via upload

---
+++ aula 5 exercícios/exercício 1/script.js
@@ -0,0 +1,5 @@
+function calcula(){
+    //console.log("Entrei")
+    var nome= prompt('Digite seu nome:')
+    alert('Bem vindo (a), ' + nome)
+}
---
+++ aula 5 exercícios/exercício 2/script.js
@@ -0,0 +1,9 @@
+function info(){
+    //console.log("Entrei")
+    var nome= prompt('Digite seu nome:')
+
+    texto.innerHTML = Olá
+
+    //document.writeln('<p> Olá ' + nome + '</p>')
+}
+
