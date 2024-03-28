c13399fd7cc38dfbbee504bd93bc1d5c0ff0ca89
carolinalibarino <carollibarino2@gmail.com>
2024-03-28 11:25:16 -0300

    modo amarelo e azul

--- aula 6 manipulando css/script.js
+++ aula 6 manipulando css/script.js
@@ -9,9 +9,16 @@ let lightbutton = document.querySelector('#lightbutton')
 
 let pinkbutton = document.querySelector('#pinkbutton')
 
+let yellowbutton = document.querySelector('#yellowbutton')
+
+let bluebutton = document.querySelector('#bluebutton')
+
 darkbutton.addEventListener('click', modoDark)
 lightbutton.addEventListener('click', modoLight)
 pinkbutton.addEventListener('click', modoPink)
+yellowbutton.addEventListener('click', modoYellow)
+bluebutton.addEventListener('click', modoBlue)
+
 
 function modoDark() {
     event.preventDefault();
@@ -19,6 +26,8 @@ function modoDark() {
     tela.classList.add("dark");
     tela.classList.remove("light");
     tela.classList.remove("pink");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
 }
 
 function modoLight() {
@@ -27,6 +36,8 @@ function modoLight() {
     tela.classList.add("light");
     tela.classList.remove("dark");
     tela.classList.remove("pink");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
 }
 
 function modoPink(){
@@ -35,4 +46,26 @@ function modoPink(){
     tela.classList.add("pink");
     tela.classList.remove("dark");
     tela.classList.remove("light");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
+}
+
+function modoYellow(){
+    event.preventDefault();
+    console.log('modo yellow')
+    tela.classList.add("yellow");
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("pink");
+    tela.classList.remove("blue");
+}
+
+function modoBlue(){
+    event.preventDefault();
+    console.log('modo yellow')
+    tela.classList.add("blue");
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("pink");
+    tela.classList.remove("yellow");
 }
